56ac50c8cc481f5af681b028d8c4a55e924f8b59
Nate Finch <nate@strattic.com>
2022-02-14 11:20:03 -0600

    update index for intro

---
+++ wordpress/1-intro-to-wp.php
@@ -0,0 +1,7 @@
+<?php
+
+// 1. set up LocalWP
+// 2. download extra PHP versions
+// 3. show PHP on Mac
+// 4. can use https://sandbox.onlinephpfunctions.com/
+// 5. can use Virtual Box to run Linux
