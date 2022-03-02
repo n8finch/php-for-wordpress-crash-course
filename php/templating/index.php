b2f0656740e5c91ed19e466a79d417090707a59a
Nate Finch <nate@strattic.com>
2022-03-01 20:39:48 -0600

    update the includes

--- php/templating/index.php
+++ php/templating/index.php
@@ -1,9 +1,9 @@
 <?php
 // Keep this opening tag!
-include './php/templating/data.php';
-include_once './php/templating/header.php';
-require './php/templating/content.php';
-require_once './php/templating/footer.php';
+include './php/templating/data.php'; // Execution will continue of the file can't be found.
+include_once './php/templating/header.php'; // If the file has already been included, it won't do it again.
+require './php/templating/content.php'; // Will throw a fatal error if the file
+require_once './php/templating/footer.php'; // If the file has already been required, it won't do it again.
 
 get_header();
 get_content();
