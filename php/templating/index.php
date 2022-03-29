9e6635c21f45d2f8a80a629324ec11ab6c2bd538
Nate Finch <nate@strattic.com>
2022-03-29 13:50:22 -0500

    update comment for includes

--- php/templating/index.php
+++ php/templating/index.php
@@ -2,7 +2,7 @@
 // Keep this opening tag!
 include './php/templating/data.php'; // Execution will continue of the file can't be found.
 include_once './php/templating/header.php'; // If the file has already been included, it won't do it again.
-require './php/templating/content.php'; // Will throw a fatal error if the file
+require './php/templating/content.php'; // Will throw a fatal error if the file can't be found.
 require_once './php/templating/footer.php'; // If the file has already been required, it won't do it again.
 
 get_header();
